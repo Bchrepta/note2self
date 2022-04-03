6d24eed66c7d3d3efa2a399855b3aea186e2312d
Ben <ben.chrepta@yahoo.com>
2022-04-03 17:16:06 -0400

    Delete src/components directory

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from "./About";
-export { default as Notes } from "./Notes";
-export { default as VideoUpload } from "./VideoUpload";
-export { default as Home } from "./Home";
-export { default as NavBar } from "./NavBar";
-export { default as Footer } from "./Footer";
