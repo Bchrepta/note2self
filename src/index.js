c949d1a2636d5a187d2bc8cfdcce646cb5fd6199
Ben <ben.chrepta@yahoo.com>
2022-04-03 17:17:57 -0400

    new file upload

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {
-  About,
-  Notes,
-  VideoUpload,
-  Home,
-  NavBar,
-  Footer,
-} from "./components"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Router>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/vid_upload" element={<VideoUpload />} />
-      <Route path="/notes" element={<Notes />} />
-    </Routes>
-    <Footer />
-  </Router>,
-  rootElement
+  <App />,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
